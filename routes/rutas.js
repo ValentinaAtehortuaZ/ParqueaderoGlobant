f3e797103745d60fea04045035c94bd4685b57c9
401 <401@academicos.cesde.edu.co>
2022-07-18 15:11:30 -0500

    Se ha actualizado el repositorio

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,27 +1,35 @@
 import express from 'express'
 
+import {ControladorAdministrador} from '../controllers/ControladorAdministrador.js'
+import {ControladorCelda} from '../controllers/ControladorCelda.js'
+import {ControladorVehiculo} from '../controllers/ControladorVehiculo.js'
+
+let controladorAdministrador = new ControladorAdministrador()
+let controladorCelda = new ControladorCelda()
+let controladorVehiculo = new ControladorVehiculo()
+
 export let rutas=express.Router()
 
 //Rutas administradores
 
-let administrador = new administrador()
 
-rutas.get('/api/v1/parqueadero/admins/', administrador.buscarTodos)
-rutas.get('/api/v1/parqueadero/admin/:id/', administrador.buscarPorId)
-rutas.post('/api/v1/parqueadero/admin/globantValen',administrador.insertar)
-rutas.put('/api/v1/parqueadero/admin/:id/', administrador.editar)
+
+rutas.get('/api/v1/parqueadero/admins/', controladorAdministrador.buscar)
+rutas.get('/api/v1/parqueadero/admin/:id/', controladorAdministrador.buscarAdministrador)
+rutas.post('/api/v1/parqueadero/admin/globantValen',controladorAdministrador.agregarAdministrador)
+rutas.put('/api/v1/parqueadero/admin/:id/', controladorAdministrador.editarAdministrador)
 
 
 //Rutas celdas
 
-rutas.get('/api/v1/parqueadero/celdas/', administrador.buscarTodos)
-rutas.get('/api/v1/parqueadero/celda/:id/', administrador.buscarPorId)
-rutas.post('/api/v1/parqueadero/celda/',administrador.insertar)
-rutas.put('/api/v1/parqueadero/celda/:id/', administrador.editar)
+rutas.get('/api/v1/parqueadero/celdas/', controladorCelda.buscar)
+rutas.get('/api/v1/parqueadero/celda/:id/', controladorCelda.buscarCelda)
+rutas.post('/api/v1/parqueadero/celda/',controladorCelda.agregarCelda)
+rutas.put('/api/v1/parqueadero/celda/:id/', controladorCelda.editarCelda)
 
 //Rutas vehiculos
 
-rutas.get('/api/v1/parqueadero/vehiculos/', administrador.buscarTodos)
-rutas.get('/api/v1/parqueadero/vehiculo/:id/', administrador.buscarPorId)
-rutas.post('/api/v1/parqueadero/vehiculo/',administrador.insertar)
-rutas.put('/api/v1/parqueadero/vehiculo/:id/', administrador.editar)
+rutas.get('/api/v1/parqueadero/vehiculos/', controladorVehiculo.buscar)
+rutas.get('/api/v1/parqueadero/vehiculo/:id/', controladorVehiculo.buscarVehiculo)
+rutas.post('/api/v1/parqueadero/vehiculo/',controladorVehiculo.agregarVehiculo)
+rutas.put('/api/v1/parqueadero/vehiculo/:id/', controladorVehiculo.editarVehiculo)
